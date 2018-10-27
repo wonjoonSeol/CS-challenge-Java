dce4138352b954f79d8678b7524fa4384652be12
Wonjoon Seol <wonjoon.seol@gmail.com>
2018-10-28 00:28:16 +0100

    complete q2

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -5,11 +5,11 @@ public class Question4 {
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
 	    if (rows.length < 1) return 0;
 	    int minTime = 0;
-        int conseq;
-        int current;
+        boolean isFound = false;
+
 	    for (int i = 0; i < rows.length; i++) {
-            conseq = numberMachines;
-            current = 0;
+            int conseq = numberMachines;
+            int current = 0;
 	        for (int j = 0; j < rows[i].length; i++) {
 	            if (rows[i][j].equals("X")) {
 	                conseq = numberMachines;
@@ -17,10 +17,13 @@ public class Question4 {
 	                conseq--;
 	                current += Integer.parseInt(rows[i][j]);
                 }
-	            if (conseq == 0 && current < minTime) minTime = current;
-
+	            if (conseq == 0) {
+	            	isFound = true;
+					if (current < minTime) minTime = current;
+				}
             }
         }
-        return minTime;
+        if (isFound) return minTime;
+        return -1;
 	}
 }
