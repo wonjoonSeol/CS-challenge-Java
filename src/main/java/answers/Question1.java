c47c7fabf4bc4fc8378e69cd7818f0b87186e62f
Wonjoon Seol <wonjoon.seol@gmail.com>
2018-10-27 02:30:40 +0100

    question1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,16 @@
 package answers;
 
 public class Question1 {
-
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		if (portfolios.length == 1) return portfolios[0];
+	    int portfolioC = 0;
+	    int current;
+	    for (int i = 0; i < portfolios.length; i++) {
+	    	for (int j = i; j < portfolios.length; j++) {
+				current = portfolios[i] ^ portfolios[j];
+				if (portfolioC < current) portfolioC = current;
+			}
+		}
+		return portfolioC;
 	}
-
 }
