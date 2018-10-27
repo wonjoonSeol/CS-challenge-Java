aa5e1d7eeccc134d5f6c12787ec12403bf66bf8c
Wonjoon Seol <wonjoon.seol@gmail.com>
2018-10-28 00:05:45 +0100

    complete q4

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -3,8 +3,24 @@ package answers;
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
-	}
+	    if (rows.length < 1) return 0;
+	    int minTime = 0;
+        int conseq;
+        int current;
+	    for (int i = 0; i < rows.length; i++) {
+            conseq = numberMachines;
+            current = 0;
+	        for (int j = 0; j < rows[i].length; i++) {
+	            if (rows[i][j].equals("X")) {
+	                conseq = numberMachines;
+                } else {
+	                conseq--;
+	                current += Integer.parseInt(rows[i][j]);
+                }
+	            if (conseq == 0 && current < minTime) minTime = current;
 
+            }
+        }
+        return minTime;
+	}
 }
