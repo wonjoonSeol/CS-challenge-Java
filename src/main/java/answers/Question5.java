c55e65e1ce03ff9e2599945725b81a3e91f31a23
Wonjoon Seol <wonjoon.seol@gmail.com>
2018-10-28 03:28:39 +0000

    q5 non allocated case

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -6,14 +6,19 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		Arrays.sort(allowedAllocations);
+		boolean isAllocated = false;
 //		System.out.println(Arrays.toString(allowedAllocations));
 		int num = 0;
 		for (int i = allowedAllocations.length - 1; i >= 0; i--) {
 			totalValue -= allowedAllocations[i];
 			num++;
-			if (totalValue <= 0) break;
+			if (totalValue <= 0) {
+				isAllocated = true;
+				break;
+			}
 		}
-		return num;
+		if (isAllocated) return num;
+		return 0;
 	}
 
 	// memoisation
