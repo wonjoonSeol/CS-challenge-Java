dac449dedaf059d0ac6eb02c3aa650053a7a68c9
Wonjoon Seol <wonjoon.seol@gmail.com>
2018-10-30 22:00:22 +0000

    preliminary q2

--- src/main/java/answers/Question2.java
+++ src/main/java/answers/Question2.java
@@ -1,19 +1,56 @@
 package answers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Question2 {
 
 	public static int equallyBalancedCashFlow(int[] cashflowIn, int[] cashflowOut) {
+		Arrays.sort(cashflowIn);
+		Arrays.sort(cashflowOut);
 
-
-
-		return -1;
+		System.out.println("Sorted\ncashflow IN " + Arrays.toString(cashflowIn));
+		System.out.println("Cashflow Out " + Arrays.toString(cashflowOut));
+		if (cashflowIn.length < cashflowOut.length) {
+			return smallestDifference(cashflowIn, cashflowOut, new ArrayList<Integer>(), new ArrayList<Integer>());
+		} else {
+			return smallestDifference(cashflowOut, cashflowIn, new ArrayList<Integer>(), new ArrayList<Integer>());
+		}
 	}
 
-	public static int smallestDifference(int[] cashflowIn, int[] cashflowOut, int difference, int[] memo) {
+	public static int smallestDifference(int[] shorter, int[] longer, List<Integer> positives, List<Integer> negatives) {
+		for (int firstNum : shorter) {
+			int min = Integer.MAX_VALUE;
+			for (int secondNum : longer) {
+				int difference = firstNum - secondNum;
+				if (Math.abs(difference) < min) {
+					min = difference;
+				} else {
+					break;
+				}
+			}
+			if (min >= 0) {
+				positives.add(min);
+			} else {
+				negatives.add(min);
+			}
+		}
+		positives.sort(Integer::compareTo);
+		negatives.sort(Collections.reverseOrder());
 
+		System.out.println("Positives " + positives);
+		System.out.println("Negatives " + negatives);
 
-		return -1;
+		int min = Integer.MAX_VALUE;
+		for (int positive : positives) {
+		    for (int negative : negatives) {
+		    	int difference = Math.abs(positive + negative);
+				if (difference == 0) return 0;
+				if (difference < min) min = difference;
+			}
+		}
+		return min;
 	}
 }
